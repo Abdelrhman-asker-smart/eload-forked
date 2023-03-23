5d96bd400cb130d6fbd9071cd04407bc65eba5a1
Mahmoud Ahmad | SMART <mahmad@smart.sa>
2023-03-23 15:05:07 +0200

    updating the rewards

--- src/Components/Rewards/Rewards.jsx
+++ src/Components/Rewards/Rewards.jsx
@@ -1,21 +1,53 @@
 import React from "react";
-import "./rewards.css";
-import { useState } from "react";
+import { useCookies } from "react-cookie";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import CustomSelect from '../CustomeSelect/CustomeSelect';
+import Select from 'react-select';
+import { fetchPromotionList } from "../../redux/listPromotion";
+import "./rewards.css";
 
 const Rewards = () => {
+  const dispatch = useDispatch();
+  const [promotionList, setpromotionList] = useState([]);
+  const [cookie] = useCookies(["eload_token"]);
+  const data = promotionList.map((item, index) => {
+    return {
+      id: item.id,
+      name: item.name,
+    };
+  });
+
+  useEffect(() => {
+    dispatch(fetchPromotionList({ token: cookie.eload_token }))
+    .then((res) => {
+      console.log(res, "response from api");
+      const data = res.payload.data;
+      setpromotionList(data);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+  }, []);
+
   const [count1, setCount1] = useState(1);
   const [count2, setCount2] = useState(1);
   const [count3, setCount3] = useState(1);
 
     /// dropdown
-    const optionsRewards = [
-      { value: "Number of trips", label: "Number of trips" },
-      { value: "Number of trips covered in a specific time", label: "Number of trips covered in a specific time" },
-      { value: "Number of trips to a certain destination", label: "Number of trips to a certain destination" },
-      { value: "EgyNumber of trips based on a specific truck typept", label: "Number of trips based on a specific truck type" },
-
+    const typesOptions = [
+      { value: "fixed", label: "Fixed" },
+      { value: "percentage", label: "Percentage" },
     ];
+
+    const optionsRewards = [
+      { value: "Start time", label: "Start time" },
+      { value: "End time", label: "End time" },
+      { value: "Source", label: "Source" },
+      { value: "Destination", label: "Destination" },
+      { value: "Truck type", label: "Truck type" },
+    ];
+
     const handleSelect = (selectedOption) => {
       console.log("Selected option:", selectedOption);
     };
@@ -66,21 +98,49 @@ const Rewards = () => {
             </div>
           </div>
           <div className="col-md-4 justfy-content-end pt-3 ">
-          <CustomSelect options={optionsRewards} onSelect={handleSelect}  />
-
+          {/* <CustomSelect options={optionsRewards} onSelect={handleSelect}  /> */}
+          <Select
+            classNamePrefix="select"
+            className="basic-multi-select"
+            isMulti
+            isDisabled={false}
+            isLoading={false}
+            isClearable={false}
+            isRtl={false}
+            isSearchable={true}
+            name="rewards_options"
+            options={optionsRewards}
+          />
           </div>
         </div>
         {/* secound-row */}
         <div className="row py-5 align-items-center">
           <div className="col-md-4 input-side">
             <label htmlFor="address">
-              Shipment value<span>*</span>
+              Reward Type<span>*</span>
+            </label>
+            <Select
+              classNamePrefix="select"
+              className="basic-multi-select mt-10"
+              isMulti={false}
+              isDisabled={false}
+              isLoading={false}
+              isClearable={false}
+              isRtl={false}
+              isSearchable={true}
+              name="rewards_options"
+              options={typesOptions}
+            />
+          </div>
+          <div className="col-md-4 input-side">
+            <label htmlFor="address">
+              Reward Amount<span>*</span>
             </label>
             <input type="text" placeholder="ie. 500 SAR" />
           </div>
           <div className="col-md-4 numbers-box text-center">
             <label htmlFor="address">
-              Shipment value<span>*</span>
+              Number of trips<span>*</span>
             </label>
             <div className="qty">
               <span
@@ -126,182 +186,6 @@ const Rewards = () => {
         </div>
       </div>
       <hr />
-      {/* section-2 */}
-      <div className="row px-4 py-2">
-        {/* first-row */}
-        <div className="row justify-content-between">
-          <div className="col-md-6 align-items-center">
-            <div className="icon-box d-flex align-items-center">
-              <svg
-                width="71"
-                height="71"
-                viewBox="0 0 71 71"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M62.8655 54.6404L57.9843 55.7942C56.8897 56.0604 56.0318 56.8888 55.7951 57.9833L54.7597 62.3321C54.1976 64.6987 51.1801 65.4383 49.6122 63.5746L40.7668 53.3979C40.0568 52.5696 40.4414 51.2679 41.5064 51.0017C46.7426 49.7296 51.4464 46.8008 54.9076 42.6296C55.4697 41.9492 56.4755 41.8604 57.0968 42.4817L63.6643 49.0492C65.9126 51.2975 65.1139 54.1079 62.8655 54.6404Z"
-                  fill="#FD3D00"
-                />
-                <path
-                  d="M7.98762 54.6404L12.8689 55.7942C13.9634 56.0604 14.8214 56.8888 15.058 57.9833L16.0934 62.3321C16.6555 64.6987 19.673 65.4383 21.2409 63.5746L30.0864 53.3979C30.7964 52.5696 30.4118 51.2679 29.3468 51.0017C24.1105 49.7296 19.4068 46.8008 15.9455 42.6296C15.3834 41.9492 14.3776 41.8604 13.7564 42.4817L7.18887 49.0492C4.94053 51.2975 5.73928 54.1079 7.98762 54.6404Z"
-                  fill="#FD3D00"
-                />
-                <path
-                  d="M35.5001 5.91699C24.0513 5.91699 14.7917 15.1766 14.7917 26.6253C14.7917 30.9149 16.0638 34.8495 18.253 38.1332C21.448 42.8666 26.5067 46.2095 32.3938 47.0674C33.3997 47.2449 34.4351 47.3337 35.5001 47.3337C36.5651 47.3337 37.6005 47.2449 38.6063 47.0674C44.4934 46.2095 49.5522 42.8666 52.7472 38.1332C54.9363 34.8495 56.2084 30.9149 56.2084 26.6253C56.2084 15.1766 46.9488 5.91699 35.5001 5.91699ZM44.5526 25.9745L42.0972 28.4299C41.683 28.8441 41.4463 29.6428 41.5943 30.2345L42.3043 33.2816C42.8663 35.6778 41.5942 36.6245 39.4643 35.3524L36.5059 33.607C35.9734 33.2816 35.0859 33.2816 34.5534 33.607L31.5951 35.3524C29.4651 36.5949 28.193 35.6778 28.7551 33.2816L29.4651 30.2345C29.5834 29.6724 29.3763 28.8441 28.9622 28.4299L26.4476 25.9745C24.998 24.5249 25.4713 23.0753 27.483 22.7499L30.6484 22.2174C31.1809 22.1287 31.8022 21.6553 32.0388 21.182L33.7842 17.6912C34.7309 15.7978 36.2692 15.7978 37.2159 17.6912L38.9613 21.182C39.198 21.6553 39.8192 22.1287 40.3813 22.2174L43.5468 22.7499C45.5288 23.0753 46.0022 24.5249 44.5526 25.9745Z"
-                  fill="#FD3D00"
-                />
-              </svg>
-
-              <h4>Second Reward</h4>
-            </div>
-          </div>
-          <div className="col-md-4 justfy-content-end pt-3 ">
-          <CustomSelect options={optionsRewards} onSelect={handleSelect}  />
-          </div>
-        </div>
-        {/* secound-row */}
-        <div className="row py-5 align-items-center">
-          <div className="col-md-4 input-side">
-            <label htmlFor="address">
-              Shipment value<span>*</span>
-            </label>
-            <input type="text" placeholder="ie. 500 SAR" />
-          </div>
-          <div className="col-md-4 numbers-box text-center">
-            <label htmlFor="address">
-              Shipment value<span>*</span>
-            </label>
-            <div className="qty">
-              <span
-                onClick={() => {
-                  if (count2 > 1) {
-                    setCount2((prevState) => prevState - 1);
-                  }
-                }}
-                className="minus"
-              >
-                -
-              </span>
-              <input
-                type="number"
-                className="count"
-                name="qty"
-                value={count2}
-              />
-              <span
-                onClick={() => {
-                  if (count2 < 50) {
-                    setCount2((prevState) => prevState + 1);
-                  }
-                }}
-                className="plus "
-              >
-                +
-              </span>
-            </div>
-          </div>
-          <div className="col-md-4 d-flex justify-content-center mt-4">
-            <button
-              className="btn add-btn"
-              style={{ padding: "6px 20px", height: "52%", width: "46%" }}
-              data-bs-toggle="modal"
-              href="#exampleModalToggle"
-            >
-              Save
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <hr />
-      {/* section-3 */}
-      <div className="row px-4 py-2">
-        {/* first-row */}
-        <div className="row justify-content-between">
-          <div className="col-md-6 align-items-center">
-            <div className="icon-box d-flex align-items-center">
-              <svg
-                width="71"
-                height="71"
-                viewBox="0 0 71 71"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M62.8655 54.6404L57.9843 55.7942C56.8897 56.0604 56.0318 56.8888 55.7951 57.9833L54.7597 62.3321C54.1976 64.6987 51.1801 65.4383 49.6122 63.5746L40.7668 53.3979C40.0568 52.5696 40.4414 51.2679 41.5064 51.0017C46.7426 49.7296 51.4464 46.8008 54.9076 42.6296C55.4697 41.9492 56.4755 41.8604 57.0968 42.4817L63.6643 49.0492C65.9126 51.2975 65.1139 54.1079 62.8655 54.6404Z"
-                  fill="#ED13CA"
-                />
-                <path
-                  d="M7.98762 54.6404L12.8689 55.7942C13.9634 56.0604 14.8214 56.8888 15.058 57.9833L16.0934 62.3321C16.6555 64.6987 19.673 65.4383 21.2409 63.5746L30.0864 53.3979C30.7964 52.5696 30.4118 51.2679 29.3468 51.0017C24.1105 49.7296 19.4068 46.8008 15.9455 42.6296C15.3834 41.9492 14.3776 41.8604 13.7564 42.4817L7.18887 49.0492C4.94053 51.2975 5.73928 54.1079 7.98762 54.6404Z"
-                  fill="#ED13CA"
-                />
-                <path
-                  d="M35.5001 5.91699C24.0513 5.91699 14.7917 15.1766 14.7917 26.6253C14.7917 30.9149 16.0638 34.8495 18.253 38.1332C21.448 42.8666 26.5067 46.2095 32.3938 47.0674C33.3997 47.2449 34.4351 47.3337 35.5001 47.3337C36.5651 47.3337 37.6005 47.2449 38.6063 47.0674C44.4934 46.2095 49.5522 42.8666 52.7472 38.1332C54.9363 34.8495 56.2084 30.9149 56.2084 26.6253C56.2084 15.1766 46.9488 5.91699 35.5001 5.91699ZM44.5526 25.9745L42.0972 28.4299C41.683 28.8441 41.4463 29.6428 41.5943 30.2345L42.3043 33.2816C42.8663 35.6778 41.5942 36.6245 39.4643 35.3524L36.5059 33.607C35.9734 33.2816 35.0859 33.2816 34.5534 33.607L31.5951 35.3524C29.4651 36.5949 28.193 35.6778 28.7551 33.2816L29.4651 30.2345C29.5834 29.6724 29.3763 28.8441 28.9622 28.4299L26.4476 25.9745C24.998 24.5249 25.4713 23.0753 27.483 22.7499L30.6484 22.2174C31.1809 22.1287 31.8022 21.6553 32.0388 21.182L33.7842 17.6912C34.7309 15.7978 36.2692 15.7978 37.2159 17.6912L38.9613 21.182C39.198 21.6553 39.8192 22.1287 40.3813 22.2174L43.5468 22.7499C45.5288 23.0753 46.0022 24.5249 44.5526 25.9745Z"
-                  fill="#ED13CA"
-                />
-              </svg>
-
-              <h4>Second Reward</h4>
-            </div>
-          </div>
-          <div className="col-md-4 justfy-content-end pt-3 ">
-          <CustomSelect options={optionsRewards} onSelect={handleSelect}  />
-          </div>
-        </div>
-        {/* secound-row */}
-        <div className="row py-5 align-items-center">
-          <div className="col-md-4 input-side">
-            <label htmlFor="address">
-              Shipment value<span>*</span>
-            </label>
-            <input type="text" placeholder="ie. 500 SAR" />
-          </div>
-          <div className="col-md-4 numbers-box text-center">
-            <label htmlFor="address">
-              Shipment value<span>*</span>
-            </label>
-            <div className="qty">
-              <span
-                onClick={() => {
-                  if (count3 > 1) {
-                    setCount3((prevState) => prevState - 1);
-                  }
-                }}
-                className="minus"
-              >
-                -
-              </span>
-              <input
-                type="number"
-                className="count"
-                name="qty"
-                value={count3}
-              />
-              <span
-                onClick={() => {
-                  if (count3 < 50) {
-                    setCount3((prevState) => prevState + 1);
-                  }
-                }}
-                className="plus "
-              >
-                +
-              </span>
-            </div>
-          </div>
-          <div className="col-md-4 d-flex justify-content-center mt-4">
-            <button
-              className="btn add-btn"
-              style={{ padding: "6px 20px", height: "52%", width: "46%" }}
-              data-bs-toggle="modal"
-              href="#exampleModalToggle"
-            >
-              Save
-            </button>
-          </div>
-        </div>
-      </div>
 
               {/* modal */}
               <div
