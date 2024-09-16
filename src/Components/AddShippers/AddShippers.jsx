65674f9d6d6b7f38c7a58bedde71eae9beb5b6d0
abdelrhman Asker <aasker@smart.sa>
2024-09-16 15:58:53 +0300

    editsEmailHandlerWithJoi

--- src/Components/AddShippers/AddShippers.jsx
+++ src/Components/AddShippers/AddShippers.jsx
@@ -70,7 +70,7 @@ const AddShippers = () => {
       name: Joi.string().required(),
       // password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
       email: Joi.string()
-        .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+        .email({ minDomainSegments: 2, tlds: { allow: false } })
         .required(),
       password: Joi.string().min(6).required(),
       confirm_password: Joi.string()
